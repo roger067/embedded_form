24bc97d0e7e61c2b3dd9f4d6db478954f234087c
roger067 <rosestolato.rogerio@gmail.com>
2024-10-09 22:35:08 -0300

    test: config setup ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
 /// <reference types="vitest/config" />
 
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
-import { shadowStyle } from 'vite-plugin-shadow-style';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import { shadowStyle } from "vite-plugin-shadow-style";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact(), cssInjectedByJsPlugin(), shadowStyle()],
   build: {
     rollupOptions: {
-      input: { 'pingback-form': '/src/wc/pingback-form.tsx' },
+      input: { "pingback-form": "/src/wc/pingback-form.tsx" },
       output: {
-        entryFileNames: 'pingback-form.js',
+        entryFileNames: "pingback-form.js",
       },
     },
   },
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./vitest.setup.ts"],
   },
 });
